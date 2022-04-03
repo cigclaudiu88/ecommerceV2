7cde6767ce564770edbffe0f8afbaabd3cef7e93
cigclaudiu88 <claudiu.cigusevici@gmail.com>
2022-04-03 10:44:07 +0300

    Ai uitat parola + Resetare Parola Design
    1. Actualizat pagina Ai uitat parola?
    
    2. Actualizat pagina Resetare Parola
    
    3. Test : Ai uitat parola -> Resetare Parola -> Autentificare -> DONE

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,102 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!doctype html>
+<html class="no-js" lang="en">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>eShop Autentificare</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('backend/images/favicon.ico') }}">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    <!-- CSS
+ ============================================ -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/bootstrap.min.css') }}">
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/material-design-iconic-font.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/cryptocurrency-icons.css') }}">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/plugins/plugins.css') }}">
+
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/helper.css') }}">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+
+</head>
+
+<body class="skin-dark">
+
+    <div class="main-wrapper">
+
+        <!-- Content Body Start -->
+        <div class="content-body m-0 p-0">
+
+            <div class="login-register-wrap">
+                <div class="row">
+
+                    <div class="d-flex align-self-center justify-content-center order-2 order-lg-1 col-lg-5 col-12">
+                        <div class="login-register-form-wrap">
+
+                            <div class="content">
+                                <h1>Resetare parola cont</h1>
+                                <p>Ai uitat parola? Nici o problema. Completeaza formularul cu o adresa de mail si vei
+                                    primi un mail cu un link de resetare parola!</p>
+                            </div>
+
+                            <div class="login-register-form">
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+
+                                    <div class="row">
+                                        <div class="col-12 mb-20"><input class="form-control" type="email"
+                                                id="email" name="email" placeholder="Adresa de email."></div>
+                                        <div class="col-12 mt-10"><button
+                                                class="button button-primary button-outline">Reseteaza parola!</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="login-register-bg order-1 order-lg-2 col-lg-7 col-12">
+                        <div class="content">
+                            <h1>Sign in</h1>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        @endif
 
-        <x-jet-validation-errors class="mb-4" />
+        </div><!-- Content Body End -->
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+    </div>
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    <!-- JS
+============================================ -->
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    <!-- Global Vendor, plugins & Activation JS -->
+    <script src="{{ asset('backend/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('backend/js/vendor/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('backend/js/vendor/popper.min.js') }}"></script>
+    <script src="{{ asset('backend/js/vendor/bootstrap.min.js') }}"></script>
+    <!--Plugins JS-->
+    <script src="{{ asset('backend/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('backend/js/plugins/tippy4.min.js.js') }}"></script>
+    <!--Main JS-->
+    <script src="{{ asset('backend/js/main.js') }}"></script>
+
+</body>
+
+</html>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,122 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!doctype html>
+<html class="no-js" lang="en">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>eShop Autentificare</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('backend/images/favicon.ico') }}">
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+    <!-- CSS
+ ============================================ -->
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/bootstrap.min.css') }}">
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/material-design-iconic-font.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/vendor/cryptocurrency-icons.css') }}">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/plugins/plugins.css') }}">
+
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/helper.css') }}">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+
+</head>
+
+<body class="skin-dark">
+
+    <div class="main-wrapper">
+
+        <!-- Content Body Start -->
+        <div class="content-body m-0 p-0">
+
+            <div class="login-register-wrap">
+                <div class="row">
+
+                    <div class="d-flex align-self-center justify-content-center order-2 order-lg-1 col-lg-5 col-12">
+                        <div class="login-register-form-wrap">
+
+                            <div class="content">
+                                <h1>Resetare parola</h1>
+                            </div>
+
+                            <div class="login-register-form">
+                                <form method="POST" action="{{ route('password.update') }}">
+                                    @csrf
+
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                    <div class="row">
+                                        <div class="col-12 mb-20"><input class="form-control" type="email"
+                                                id="email" name="email" :value="old('email', $request->email)" required
+                                                autofocus placeholder="Adresa de Email">
+                                            @error('password_confirmation')
+                                                <span class="invalid-feedback" role="alert"></span>
+                                                <strong class="text-danger">{{ $message }}</strong>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-12 mb-20"><input class="form-control" type="password"
+                                                id="password" name="password" placeholder="Parola Noua">
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert"></span>
+                                                <strong class="text-danger">{{ $message }}</strong>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-12 mb-20"><input class="form-control" type="password"
+                                                id="password_confirmation" name="password_confirmation"
+                                                placeholder="Confirmare Parola">
+                                            @error('password_confirmation')
+                                                <span class="invalid-feedback" role="alert"></span>
+                                                <strong class="text-danger">{{ $message }}</strong>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-12 mt-10"><button
+                                                class="button button-primary button-outline">Schimbare parola</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="login-register-bg order-1 order-lg-2 col-lg-7 col-12">
+                    </div>
+
+                </div>
             </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+        </div><!-- Content Body End -->
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    <!-- JS
+============================================ -->
+
+    <!-- Global Vendor, plugins & Activation JS -->
+    <script src="{{ asset('backend/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('backend/js/vendor/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('backend/js/vendor/popper.min.js') }}"></script>
+    <script src="{{ asset('backend/js/vendor/bootstrap.min.js') }}"></script>
+    <!--Plugins JS-->
+    <script src="{{ asset('backend/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('backend/js/plugins/tippy4.min.js.js') }}"></script>
+    <!--Main JS-->
+    <script src="{{ asset('backend/js/main.js') }}"></script>
+
+</body>
+
+</html>
