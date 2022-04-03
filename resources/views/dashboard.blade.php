4354320f2fefacf832cacdb5c2d35fc620dc9baa
cigclaudiu88 <claudiu.cigusevici@gmail.com>
2022-04-03 11:30:25 +0300

    User Profile Design - Part 1
    1. User Profile Design - Meniu Design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,181 @@
-<x-app-layout>
+@extends('frontend.main_master')
+@section('content')
+    <!-- my account start  -->
+    <section class="main_content_area">
+        <div class="container">
+            <div class="account_dashboard">
+                <div class="row">
+                    <div class="col-sm-12 col-md-3 col-lg-3">
+                        <img class="card-img-top" style="border-radius:50%"
+                            src="{{ !empty($adminData->profile_photo_path)? url('upload/admin_images/' . $adminData->profile_photo_path): url('upload/default_profile.png') }}"
+                            alt="" height="40%" width="40%"><br><br>
+                        <!-- Nav tabs -->
+                        <div class="dashboard_tab_button">
+                            <ul role="tablist" class="nav flex-column dashboard-list" id="nav-tab">
+                                <li><a href="#dashboard" data-toggle="tab" class="nav-link active">Acasa</a></li>
+                                <li><a href="#account-details" data-toggle="tab" class="nav-link">Detalii Cont</a>
+                                </li>
+                                <li><a href="#account-password" data-toggle="tab" class="nav-link">Schimba Parola</a>
+                                </li>
+                                <li> <a href="#orders" data-toggle="tab" class="nav-link">Istoric Comenzi</a></li>
+                                <li><a href="#downloads" data-toggle="tab" class="nav-link">Istoric Facturi</a></li>
+                                <li><a href="#address" data-toggle="tab" class="nav-link">Adrese</a></li>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <h1 class="font-bold">Hello....{{ Auth::user()->name }}</h1>
-        </h2>
-    </x-slot>
+                                <li><a href="login.html" class="nav-link">logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-9 col-lg-9">
+                        <!-- Tab panes -->
+                        <div class="tab-content dashboard_content">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                            <div class="tab-pane fade show active" id="dashboard">
+                                <h3>Acasa </h3>
+                                <p>Buna <strong>{{ Auth::user()->name }}</strong> Din aceasta pagina poti modifica
+                                    informatiile contului, poti schimba parola, poti vizualiza istoricul comenzilor plasate
+                                    si multe altele.</p>
+                            </div>
+
+                            <div class="tab-pane fade" id="account-details">
+                                <h3>Actualizare Detalii Cont </h3>
+
+                            </div>
+
+                            <div class="tab-pane fade" id="account-password">
+                                <h3>Account details </h3>
+                                <div class="login">
+                                    <div class="login_form_container">
+                                        <div class="account_login_form">
+                                            <form action="#">
+                                                <p>Already have an account? <a href="#">Log in instead!</a></p>
+                                                <div class="input-radio">
+                                                    <span class="custom-radio"><input type="radio" value="1"
+                                                            name="id_gender"> Mr.</span>
+                                                    <span class="custom-radio"><input type="radio" value="1"
+                                                            name="id_gender"> Mrs.</span>
+                                                </div> <br>
+                                                <label>First Name</label>
+                                                <input type="text" name="first-name">
+                                                <label>Last Name</label>
+                                                <input type="text" name="last-name">
+                                                <label>Email</label>
+                                                <input type="text" name="email-name">
+                                                <label>Password</label>
+                                                <input type="password" name="user-password">
+                                                <label>Birthdate</label>
+                                                <input type="text" placeholder="MM/DD/YYYY" value="" name="birthday">
+                                                <span class="example">
+                                                    (E.g.: 05/31/1970)
+                                                </span>
+                                                <br>
+                                                <span class="custom_checkbox">
+                                                    <input type="checkbox" value="1" name="optin">
+                                                    <label>Receive offers from our partners</label>
+                                                </span>
+                                                <br>
+                                                <span class="custom_checkbox">
+                                                    <input type="checkbox" value="1" name="newsletter">
+                                                    <label>Sign up for our newsletter<br><em>You may unsubscribe at any
+                                                            moment. For that purpose, please find our contact info in the
+                                                            legal notice.</em></label>
+                                                </span>
+                                                <div class="save_button primary_btn default_button">
+                                                    <button type="submit">Save</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="tab-pane fade" id="orders">
+                                <h3>Orders</h3>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Order</th>
+                                                <th>Date</th>
+                                                <th>Status</th>
+                                                <th>Total</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>May 10, 2018</td>
+                                                <td><span class="success">Completed</span></td>
+                                                <td>$25.00 for 1 item </td>
+                                                <td><a href="cart.html" class="view">view</a></td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>May 10, 2018</td>
+                                                <td>Processing</td>
+                                                <td>$17.00 for 1 item </td>
+                                                <td><a href="cart.html" class="view">view</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane fade" id="downloads">
+                                <h3>Downloads</h3>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Product</th>
+                                                <th>Downloads</th>
+                                                <th>Expires</th>
+                                                <th>Download</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Shopnovilla - Free Real Estate PSD Template</td>
+                                                <td>May 10, 2018</td>
+                                                <td><span class="danger">Expired</span></td>
+                                                <td><a href="#" class="view">Click Here To Download Your File</a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Organic - ecommerce html template</td>
+                                                <td>Sep 11, 2018</td>
+                                                <td>Never</td>
+                                                <td><a href="#" class="view">Click Here To Download Your File</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane" id="address">
+                                <p>The following addresses will be used on the checkout page by default.</p>
+                                <h4 class="billing-address">Billing address</h4>
+                                <a href="#" class="view">Edit</a>
+                                <p><strong>Bobby Jackson</strong></p>
+                                <address>
+                                    House #15<br>
+                                    Road #1<br>
+                                    Block #C <br>
+                                    Banasree <br>
+                                    Dhaka <br>
+                                    1212
+                                </address>
+                                <p>Bangladesh</p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </section>
+    <!-- my account end   -->
+@endsection
