00ff14250f8b5ac6471a83d9167900b3dbfab457
cigclaudiu88 <claudiu.cigusevici@gmail.com>
2022-04-17 10:13:02 +0300

    Sliders - Partea 2
    1. Actualizat pagina de vizualizare sliders + ruta slider.store -> resources\views\backend\slider\slider_view.blade.php
    
    2. Adaugat ruta slider.store -> web routes
    
    3. Adaugat functia SliderStore() -> SliderController

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Backend;
 
 // includem modelul pentru tabelul sliders
 use App\Models\Slider;
+// adaugam clasa de lucru cu imagini din Image Intervention Package
+use Intervention\Image\ImageManagerStatic as Image;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -16,5 +18,52 @@ class SliderController extends Controller
         $sliders = Slider::latest()->get();
         // returnam view-ul slider_view.blade.php si trimitem ca parametru $sliders
         return view('backend.slider.slider_view', compact('sliders'));
+    }
+
+    // functia de adaugare a unui slider
+    public function SliderStore(Request $request)
+    {
+        // validarea datelor trimise prin formularul de adaugare a unui slider
+        $request->validate(
+            [
+                'slider_title' => 'required|unique:sliders|min:6',
+                'slider_description' => 'required|min:6',
+                'slider_image' => 'required|image|mimes:jpeg,png,jpg',
+            ],
+            // mesaje erori validare campuri
+            [
+                'slider_title.required' => 'Campul titlu este necesar',
+                'slider_title.unique' => 'Titlul slider-ului trebuie sa fie unic',
+                'slider_title.min' => 'Titlul slider-ului trebuie sa contina minim 6 caractere',
+                'slider_description.required' => 'Campul descriere este necesar',
+                'slider_description.min' => 'Descrierea slider-ului trebuie sa contina minim 6 caractere',
+                'slider_image.required' => 'Campul imagine este necesar',
+                'slider_image.image' => 'Campul imagine trebuie sa fie o imagine',
+                'slider_image.mimes' => 'Campul imagine trebuie sa fie o imagine de tip JPEG, PNG sau JPG',
+            ]
+        );
+        // $image preia imaginea din formularul de inserare din campul slider_image
+        $image = $request->file('slider_image');
+        // $nume_gen genereaza un nume random pentru imaginea brandului folosind hexdec() cu uniqid()
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        // folosind Image model (image intervention package) redimenzionam imaginea $image la latimea 870 si inaltimea 370 si salvam imaginea in upload/brand/ cu numele $name_gen
+        Image::make($image)->resize(870, 370)->save('upload/slider/' . $name_gen);
+        // salvam imaginea $save_url in folderul upload/brand/ cu numele $name_gen
+        $save_url = 'upload/slider/' . $name_gen;
+
+        // inseram in tabela sliders un nou slider cu datele din formularul de inserare
+        Slider::insert([
+            'slider_title' => $request->slider_title,
+            'slider_description' => $request->slider_description,
+            'slider_image' => $save_url,
+        ]);
+
+        // afisam mesaj de notificare
+        $notification = array(
+            'message' => 'Slider adaugat cu succes!',
+            'alert-type' => 'success'
+        );
+        // redirectionam catre pagina de afisare a tuturor slider-urilor cu notificare
+        return redirect()->back()->with($notification);
     }
 }
--- resources/views/backend/slider/slider_view.blade.php
+++ resources/views/backend/slider/slider_view.blade.php
@@ -1,1 +1,119 @@
+@extends('admin.admin_master')
+@section('admin')
+    <div class="row">
 
+        <!--Default Data Table Start-->
+        <div class="col-8 mb-30">
+            <div class="box">
+                <div class="box-head">
+                    <h3 class="title">Lista Sliders</h3>
+                </div>
+                <div class="box-body">
+
+                    <table class="table table-bordered data-table data-table-default">
+                        <thead>
+                            <tr>
+                                <th>Poza Slider</th>
+                                <th>Titlu Slider</th>
+                                <th>Descriere Slider</th>
+                                <th>Status Slider</th>
+                                <th>Actiuni</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- iteram cu variabila $sliders  (SliderView() din SliderController) ca $item si afisam in tabel toate valorile din tabelul sliders --}}
+                            @foreach ($sliders as $item)
+                                <tr>
+                                    <td>
+                                        <img src="{{ asset($item->slider_image) }}" style="width: 70px; height:40px"
+                                            alt="">
+                                    </td>
+                                    <td>{{ $item->slider_title }}</td>
+                                    <td>{{ $item->slider_description }}</td>
+
+                                    <td class="text-center">
+                                        @if ($item->slider_status == 1)
+                                            <span class="badge badge-pill badge-success"> Active </span>
+                                        @else
+                                            <span class="badge badge-pill badge-danger"> Inactive </span>
+                                        @endif
+                                    </td>
+
+                                    <td width="30%">
+
+                                        <a href="" class="btn btn-success">Edit</a>
+                                        <a href="" class="btn btn-danger" id="delete">Delete</a>
+
+                                        @if ($item->status == 1)
+                                            <a href="{{ route('product.inactive', $item->id) }}"
+                                                class="btn btn-danger">Inactiv</a>
+                                        @else
+                                            <a href="{{ route('product.active', $item->id) }}"
+                                                class="btn btn-success">Activ</a>
+                                        @endif
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!--Default Data Table End-->
+
+        <div class="col-lg-4 col-12 mb-30">
+            <div class="box">
+                <div class="box-head">
+                    <h4 class="title">Adauga Slider-uri</h4>
+                </div>
+                <div class="box-body">
+                    {{-- formular de adaugare slider-uri in tabelul sliders folosind ruta slider.store si functia SliderStore() din SliderController --}}
+                    {{-- enctype pentru lucrul cu imagini si protectie csrf --}}
+                    <form method="POST" action="{{ route('slider.store') }}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row mbn-20">
+
+                            <div class="col-12 mb-20">
+                                <label for="slider_title"><strong>Titlu Slider</strong></label>
+                                <input type="text" name="slider_title" id="slider_title" class="form-control"
+                                    placeholder="Nume Brand">
+                                @error('slider_title')
+                                    <span class="text-danger"><strong>{{ $message }}</strong></span>
+                                @enderror
+                            </div>
+
+                            <div class="col-12 mb-20">
+                                <label for="slider_description"><strong>Descriere Slider</strong></label>
+                                <input type="text" name="slider_description" id="slider_description" class="form-control"
+                                    placeholder="Nume Brand">
+                                @error('slider_description')
+                                    <span class="text-danger"><strong>{{ $message }}</strong></span>
+                                @enderror
+                            </div>
+
+                            <div class="col-12 mb-20">
+                                <label for="slider_image"><strong>Poza Slider</strong></label>
+                                <input type="file" name="slider_image" id="imagedisplay" class="form-control">
+                                @error('slider_image')
+                                    <span class="text-danger"><strong>{{ $message }}</strong></span>
+                                @enderror
+                            </div>
+
+                            {{-- afisam imaginea salvata in tabela sliders --}}
+                            <div class="col-12 mb-20">
+                                <img src="" alt="" id="showImage">
+                            </div>
+
+                            <div class="col-12 mb-20">
+                                <input type="submit" value="Adauga Slider" class="button button-primary">
+                                {{-- <input type="submit" value="cancle" class="button button-danger"> --}}
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
