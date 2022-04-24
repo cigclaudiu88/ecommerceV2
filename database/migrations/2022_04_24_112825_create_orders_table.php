b26923dd41a18f3a4558ec9452c50853c45e1e7b
cigclaudiu88 <claudiu.cigusevici@gmail.com>
2022-04-24 14:51:44 +0300

    Comenzi si produse comenzi Design Baza de Date - Partea 2
    1. Creat structura tabelelor orders (comenzi) si orderitems (produse comenzi) + migrat in BD

--- database/migrations/2022_04_24_112825_create_orders_table.php
+++ database/migrations/2022_04_24_112825_create_orders_table.php
@@ -13,8 +13,42 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
+        // structura tabelului orders (comenzi)
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            // Date pentru Adresa de Livrare
+            $table->unsignedBigInteger('division_id');
+            $table->unsignedBigInteger('district_id');
+            $table->string('shipping_first_name');
+            $table->string('shipping_last_name');
+            $table->string('shipping_phone');
+            $table->string('shipping_email');
+            $table->string('shipping_street');
+            $table->string('shipping_street_number');
+            $table->string('shipping_building')->nullable();
+            $table->string('shipping_apartment')->nullable();
+            $table->text('notes')->nullable();
+            // Date aferente comenzii
+            $table->string('payment_type');
+            $table->string('payment_method')->nullable();
+            $table->string('transaction_id');
+            $table->string('currency');
+            $table->float('amount', 8, 2);
+            $table->string('order_number');
+            $table->string('invoice_no');
+            $table->string('order_date');
+            $table->string('order_month');
+            $table->string('order_year');
+            $table->string('confirmed_date')->nullable();
+            $table->string('processing_date')->nullable();
+            $table->string('picked_date')->nullable();
+            $table->string('shipped_date')->nullable();
+            $table->string('delivered_date')->nullable();
+            $table->string('cancel_date')->nullable();
+            $table->string('return_date')->nullable();
+            $table->string('return_reason')->nullable();
+            $table->string('status');
             $table->timestamps();
         });
     }
--- database/migrations/2022_04_24_113012_create_order_items_table.php
+++ database/migrations/2022_04_24_113012_create_order_items_table.php
@@ -12,9 +12,18 @@ class CreateOrderItemsTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   // structura tabelului order_items (produse comandate)
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id');
+            // referinta cheie externa 
+            // care leaga tabelul order_items (order_id) cu tabelul orders (id) stergerea unei comenzi va sterge si produsele comandate
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id');
+            // $table->string('color');
+            // $table->string('size');
+            $table->string('qty');
+            $table->float('price', 8, 2);
             $table->timestamps();
         });
     }
