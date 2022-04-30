7f5c2af56734a1fa6b5f32096db119e48efb3c1d
cigclaudiu88 <claudiu.cigusevici@gmail.com>
2022-04-30 23:13:40 +0300

    Blog - Backend - Partea 6
    1. Afisare titlu, poza, continut limitat din postarile blog + categorii de blog pe pagina de blog -> resources\views\frontend\blog\blog_list.blade.php

--- app/Http/Controllers/Frontend/HomeBlogController.php
+++ app/Http/Controllers/Frontend/HomeBlogController.php
@@ -15,7 +15,7 @@ class HomeBlogController extends Controller
         // $blogcategory preia toate categoriile din tabelul blog_post_categories
         $blogcategory = BlogPostCategory::latest()->get();
         // $blogpost preia toate posturile din tabelul blog_post
-        $blogpost = BlogPost::latest()->get();
+        $blogpost = BlogPost::with('category')->latest()->get();
         return view('frontend.blog.blog_list', compact('blogpost', 'blogcategory'));
     }
 }
--- resources/views/frontend/blog/blog_list.blade.php
+++ resources/views/frontend/blog/blog_list.blade.php
@@ -17,119 +17,43 @@
         <div class="row">
             <div class="col-lg-9 col-md-12">
                 <div class="blog_wrapper">
-
-                    <article class="single_blog">
-                        <figure>
-                            <div class="blog_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/blog-big1.jpg" alt=""></a>
-                            </div>
-                            <figcaption class="blog_content">
-                                <h4 class="post_title"><a href="blog-details.html">Blog image post</a></h4>
-                                <div class="articles_date">
-                                    <p>18/01/2020 | <a href="#">eCommerce</a> </p>
+                    @foreach ($blogpost as $blog)
+                        <article class="single_blog">
+                            <figure>
+                                <div class="blog_thumb">
+                                    <a href="blog-details.html"><img class="img-responsive"
+                                            src="{{ asset($blog->post_image) }}" alt=""></a>
                                 </div>
-                                <p class="post_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    Perspiciatis doloribus itaque nulla, ratione laboriosam quisquam dolorum expedita
-                                    libero facere eius repellat qui suscipit illum a fuga odio velit. sint laboriosam
-                                    explicabo asperiores eaque minus, magnam nesciunt.</p>
-                                <footer class="btn_more">
-                                    <a href="blog-details.html"> Read more</a>
-                                </footer>
-                            </figcaption>
-                        </figure>
-                    </article>
-
+                                <figcaption class="blog_content">
+                                    <h4 class="post_title"><a href="blog-details.html">{{ $blog->post_title }}</a>
+                                    </h4>
+                                    <div class="articles_date">
+                                        <p>{{ Carbon\Carbon::parse($blog->created_at)->diffForHumans() }} | <a
+                                                href="#">{{ $blog->category->blog_category_name }}</a> </p>
+                                    </div>
+                                    <p class="post_desc">{!! Str::limit($blog->post_details, 200) !!}</p>
+                                    <footer class="btn_more">
+                                        <a href="blog-details.html"> Afla mai multe</a>
+                                    </footer>
+                                </figcaption>
+                            </figure>
+                        </article>
+                    @endforeach
                 </div>
             </div>
             <div class="col-lg-3 col-md-12">
                 <div class="blog_sidebar_widget">
-                    <div class="widget_list widget_search">
-                        <div class="widget_title">
-                            <h3>Search</h3>
-                        </div>
-                        <form action="#">
-                            <input placeholder="Search..." type="text">
-                            <button type="submit">search</button>
-                        </form>
-                    </div>
-                    <div class="widget_list comments">
-                        <div class="widget_title">
-                            <h3>Recent Comments</h3>
-                        </div>
-                        <div class="post_wrapper">
-                            <div class="post_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/comment2.png.jpg" alt=""></a>
-                            </div>
-                            <div class="post_info">
-                                <span> <a href="#">demo</a> says:</span>
-                                <a href="blog-details.html">Quisque semper nunc</a>
-                            </div>
-                        </div>
-                        <div class="post_wrapper">
-                            <div class="post_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/comment2.png.jpg" alt=""></a>
-                            </div>
-                            <div class="post_info">
-                                <span><a href="#">admin</a> says:</span>
-                                <a href="blog-details.html">Quisque orci porta...</a>
-                            </div>
-                        </div>
-                        <div class="post_wrapper">
-                            <div class="post_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/comment2.png.jpg" alt=""></a>
-                            </div>
-                            <div class="post_info">
-                                <span><a href="#">demo</a> says:</span>
-                                <a href="blog-details.html">Quisque semper nunc</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="widget_list widget_post">
-                        <div class="widget_title">
-                            <h3>Recent Posts</h3>
-                        </div>
-                        <div class="post_wrapper">
-                            <div class="post_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/blogs1.jpg" alt=""></a>
-                            </div>
-                            <div class="post_info">
-                                <h4><a href="blog-details.html">Blog image post</a></h4>
-                                <span>March 16, 2018 </span>
-                            </div>
-                        </div>
-                        <div class="post_wrapper">
-                            <div class="post_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/blogs2.jpg" alt=""></a>
-                            </div>
-                            <div class="post_info">
-                                <h4><a href="blog-details.html">Post with Gallery</a></h4>
-                                <span>March 16, 2018 </span>
-                            </div>
-                        </div>
-                        <div class="post_wrapper">
-                            <div class="post_thumb">
-                                <a href="blog-details.html"><img src="assets/img/blog/blogs3.jpg" alt=""></a>
-                            </div>
-                            <div class="post_info">
-                                <h4><a href="blog-details.html">Post with Audio</a></h4>
-                                <span>March 16, 2018 </span>
-                            </div>
-                        </div>
-                    </div>
                     <div class="widget_list widget_categories">
                         <div class="widget_title">
-                            <h3>Categories</h3>
+                            <h3>Categorii Blog</h3>
                         </div>
-                        <ul>
-                            <li><a href="#">Audio</a></li>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">Gallery</a></li>
-                            <li><a href="#">Image</a></li>
-                            <li><a href="#">Other</a></li>
-                            <li><a href="#">Travel</a></li>
-                        </ul>
+                        @foreach ($blogcategory as $category)
+                            <ul>
+                                <li><a href="#">{{ $category->blog_category_name }}</a></li>
+                            </ul>
+                        @endforeach
                     </div>
-                    <div class="widget_list widget_tag">
+                    {{-- <div class="widget_list widget_tag">
                         <div class="widget_title">
                             <h3>Tag products</h3>
                         </div>
@@ -140,7 +64,7 @@
                                 <li><a href="#">euro</a></li>
                             </ul>
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
