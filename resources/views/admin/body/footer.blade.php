8ca5f6e9f80ac377c3c9fcf757df4c50ea82c8b9
cigclaudiu88 <101003402+cigclaudiu88@users.noreply.github.com>
2022-06-20 20:13:37 +0300

    Update footer.blade.php

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
 
     <div class="footer-copyright text-center">
-        <p class="text-body-light">Copyright © 2022.eShop UPT - Designed by Cigusevici Claudiu</p>
+        <p class="text-body-light">Copyright ©2022 - eShop UPT - Designed by Cigusevici Claudiu</p>
     </div>
 
 </div>
